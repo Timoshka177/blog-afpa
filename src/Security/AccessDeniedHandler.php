a75f64d687a508b98fd0983faf10d791e86df18c
Timoshka177 <107550627+Timoshka177@users.noreply.github.com>
2023-05-15 15:19:50 +0200

    cv

--- src/Security/AccessDeniedHandler.php
+++ src/Security/AccessDeniedHandler.php
@@ -2,12 +2,13 @@
 
 namespace App\Security;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
 
-class AccessDeniedHandler implements AccessDeniedHandlerInterface
+class AccessDeniedHandler extends AbstractController implements AccessDeniedHandlerInterface
 {
     public function handle(Request $request, AccessDeniedException $accessDeniedException): ?Response
     {
